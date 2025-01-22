165dc5f274a5fee899086363f0f8dacc2817a151
charlenelau-20 <charlenelau0120@gmail.com>
2025-01-22 17:12:26 +0800

    add echoing feature (Level-1) to ChitChat.java

--- src/main/java/ChitChat.java
+++ src/main/java/ChitChat.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class ChitChat {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         String line = "---------------------------------------------";
-        System.out.println(line + "\nHello! I'm ChitChat\nWhat can I do for you?\n" + line);
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        System.out.println(line + "\nHey there! I'm ChitChat\nWhat can I do for you?\n" + line);
+
+        String input = scan.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(line + "\n" + input + "\n" + line);
+            input = scan.nextLine();
+        }
+        System.out.println(line + "\nBye! Hope to see you again soon! :)\n" + line);
     }
 }
