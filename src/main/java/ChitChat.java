7be59fbf0d68abdf5cbe63451b0f3362051af9ef
charlenelau-20 <charlenelau0120@gmail.com>
2025-01-22 16:38:15 +0800

    add ChitChat.java

---
+++ src/main/java/ChitChat.java
@@ -0,0 +1,7 @@
+public class ChitChat {
+    public static void main(String[] args) {
+        String line = "--------------------------------------------";
+        System.out.println(line + "\nHello! I'm ChitChat\nWhat can I do for you?\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+}
