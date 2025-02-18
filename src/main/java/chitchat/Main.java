79717cafd64f9f58a315b7cfccf17d0aeba507f2
charlenelau-20 <charlenelau0120@gmail.com>
2025-02-18 18:20:58 +0800

    Main.java: Edit to have app name appear in title bar of GUI

--- src/main/java/chitchat/Main.java
+++ src/main/java/chitchat/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setChitChat(chitchat); // inject the ChitChat instance
+            stage.setTitle("ChitChat");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
